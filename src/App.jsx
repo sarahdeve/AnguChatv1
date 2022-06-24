fdc75d6b8a9df66bb9abe03fc12b151b61788b26
Sarah <55793495+sarahdeve@users.noreply.github.com>
2022-06-24 18:32:18 +0100

    Netfix cloned interface/axios

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes } from "react-router";
+import "./App.scss"
+import Home from "./pages/home/Home";
+// import Register from "./pages/register/Register";
+// import Watch from "./pages/watch/Watch";
+// import Login from "./pages/login/Login";
+
+const App = () => {
+  return (
+    // <Routes>
+      <Home/>
+    // </Routes>
+    )
+};
+
+export default App;
